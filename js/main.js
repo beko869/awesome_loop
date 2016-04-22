22c6f73c1d7090a68f840b0b445c2b88c3920d3c
bklap <benjamin.kowatsch@gmail.com>
2016-04-22 16:03:59 +0200

    randomize und check

--- js/main.js
+++ js/main.js
@@ -1,10 +1,37 @@
-var randomizeFinish = function(finish){
-  var game = finish;
-  return game;
+// Gibt eine Zufallszahl zwischen min (inklusive) und max (inklusive) zurück 
+// Die Verwendung von Math.round() erzeugt keine gleichmäßige Verteilung! 
+function getRandomInt(min, max) { 
+  return Math.floor(Math.random() * (max - min +1)) + min; 
+}
+
+var randomizeFinish = function( finish ){
+  var tmpGame = []; 
+  for(var i = 0; i < finish.length; i++){
+	tmpGame[i] = [];
+    for(var j = 0; j < finish[i].length; j++){			
+		var newObject = jQuery.extend({}, finish[i][j]);
+		newObject.orientation = getRandomInt(0,3);	
+		tmpGame[i][j] = newObject;	
+    }
+  }
+  return tmpGame;
 };
 
-var game = randomizeFinish(finish);
+var checkFinish = function( finish,game ){
+	
+	for(var i = 0; i < game.length; i++){
+		for(var j = 0; j < game[i].length; j++){
+			console.log( game[i][j].orientation, finish[i][j].orientation )
+			
+			if(game[i][j].orientation != finish[i][j].orientation){
+				return false;
+			}
+		}
+	}
+	return true;
+}
 
+var game = randomizeFinish(finish);
 
 var spin = function(x){
 	var i = x.find('.rowid').val();
@@ -34,11 +61,17 @@ var render = function(game){
   $('#game .cell').on('click', function(e){
 	  spin($(this));
 	  render(game);
+	  
+	  if( checkFinish(finish,game) ){
+		alert('Gewonnen :)');
+		
+	  }
   });
 };
 
 render(game);
 // element "nesw"
+
 
 
 
