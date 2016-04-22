7b9ddc7628df15fa156038f9cec03a9919b3afd6
MaxSt <max@stoerchle.at>
2016-04-22 20:57:57 +0200

    weitergemacht

--- js/globals.js
+++ js/globals.js
@@ -1,31 +1,106 @@
+// var finish =
+//   [
+//   [
+//     {
+//       typ: "ne",
+//       orientation: 1
+//     },
+//     {
+//       typ: "nes",
+//       orientation: 1
+//     },
+//     {
+//       typ: "ne",
+//       orientation: 2
+//     },
+//   ],
+//   [
+//     {
+//       typ: "ne",
+//       orientation: 0
+//     },
+//     {
+//       typ: "nes",
+//       orientation: 3
+//     },
+//     {
+//       typ: "ne",
+//       orientation: 3
+//     }
+//   ]
+// ];
 var finish =
-  [
-  [
-    {
-      typ: "ne",
-      orientation: 1
-    },
-    {
-      typ: "nes",
-      orientation: 1
-    },
-    {
-      typ: "ne",
-      orientation: 2
-    },
-  ],
-  [
-    {
-      typ: "ne",
-      orientation: 0
-    },
-    {
-      typ: "nes",
-      orientation: 3
-    },
-    {
-      typ: "ne",
-      orientation: 3
-    }
-  ]
-];
+    [
+      [
+        {
+          typ: "ne",
+          orientation: 1
+        },
+        {
+          typ: "ns",
+          orientation: 1
+        },
+        {
+          typ: "ns",
+          orientation: 1
+        },
+        {
+          typ: "ne",
+          orientation: 2
+        },
+      ],
+      [
+        {
+          typ: "ne",
+          orientation: 0
+        },
+        {
+          typ: "ne",
+          orientation: 2
+        },
+        {
+          typ: "ne",
+          orientation: 1
+        },
+        {
+          typ: "ne",
+          orientation: 3
+        },
+      ],
+      [
+        {
+          typ: "n",
+          orientation: 2
+        },
+        {
+          typ: "n",
+          orientation: 0
+        },
+        {
+          typ: "n",
+          orientation: 0
+        },
+        {
+          typ: "n",
+          orientation: 2
+        },
+      ],
+      [
+        {
+          typ: "n",
+          orientation: 0
+        },
+        {
+          typ: "n",
+          orientation: 1
+        },
+        {
+          typ: "n",
+          orientation: 3
+        },
+        {
+          typ: "n",
+          orientation: 0
+        }
+      ]
+    ];
--- js/main.js
+++ js/main.js
@@ -1,80 +1,89 @@
-// Gibt eine Zufallszahl zwischen min (inklusive) und max (inklusive) zurück 
-// Die Verwendung von Math.round() erzeugt keine gleichmäßige Verteilung! 
-function getRandomInt(min, max) { 
-  return Math.floor(Math.random() * (max - min +1)) + min; 
+// Gibt eine Zufallszahl zwischen min (inklusive) und max (inklusive) zurück
+// Die Verwendung von Math.round() erzeugt keine gleichmäßige Verteilung!
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min +1)) + min;
 }
 
 var randomizeFinish = function( finish ){
-  var tmpGame = []; 
+  var tmpGame = [];
   for(var i = 0; i < finish.length; i++){
 	tmpGame[i] = [];
-    for(var j = 0; j < finish[i].length; j++){			
+    for(var j = 0; j < finish[i].length; j++){
 		var newObject = jQuery.extend({}, finish[i][j]);
-		newObject.orientation = getRandomInt(0,3);	
-		tmpGame[i][j] = newObject;	
+		newObject.orientation = getRandomInt(0,3);
+		tmpGame[i][j] = newObject;
     }
   }
   return tmpGame;
 };
 
 var checkFinish = function( finish,game ){
-	
-	for(var i = 0; i < game.length; i++){
-		for(var j = 0; j < game[i].length; j++){
-			console.log( game[i][j].orientation, finish[i][j].orientation )
-			
-			if(game[i][j].orientation != finish[i][j].orientation){
-				return false;
-			}
-		}
-	}
-	return true;
+
+  for(var i = 0; i < game.length; i++){
+    for(var j = 0; j < game[i].length; j++){
+      console.log( game[i][j].orientation, finish[i][j].orientation )
+      if(game[i][j].typ != 'nesw'){//wenn nesw immer richtig
+        if(game[i][j].typ != 'ns'){
+          if(game[i][j].orientation != finish[i][j].orientation){
+            return false;
+          }
+        }else{//wenn ns immer 2 richtig
+          if((game[i][j].orientation % 2) != finish[i][j].orientation){
+            return false;
+          }
+        }
+      }
+    }
+  }
+
+  return true;
 }
 
 var game = randomizeFinish(finish);
 
 var spin = function(x){
-	var i = x.find('.rowid').val();
+  var i = x.find('.rowid').val();
     var j = x.find('.columnid').val();
     var elem = game[i][j];
     elem.orientation += 1;
-    if(elem.orientation > 3)
+    if(elem.orientation > 3){
       elem.orientation = 0;
+      x.addClass('o00');
+    }else{
+      x.addClass('o' + elem.orientation);
+    }
 }
 
 var render = function(game){
   var html = '';
-  
+
   for(var i = 0; i < game.length; i++){
     html += '<div class="row">';
     for(var j = 0; j < game[i].length; j++){
-      html += '<div class="cell">'
+      html += '<div id="' + i + 'x' + j + '" class=" cell o' + game[i][j].orientation + '">';
       html += '<input type="hidden" value="' + i + '" class="rowid" />';
       html += '<input type="hidden" value="' + j + '" class="columnid" />';
-      html += '<img class="o' + game[i][j].orientation + '" src="./img/' + game[i][j].typ + '.png"/></div>';
+      html += '<img src="./img/' + game[i][j].typ + '.png"/></div>';
     }
       html += '</div>';
   }
-  
+
   $('#game').html(html);
-  
-  $('#game .cell').on('click', function(e){
-	  spin($(this));
-	  render(game);
-	  
-	  if( checkFinish(finish,game) ){
-		alert('Gewonnen :)');
-		
-	  }
+
+    $('#game .cell').unbind('click').on('click', function(e){
+      $('#game .cell').unbind('click');
+      spin($(this));
+      setTimeout(function(){
+          render(game);
+	        if( checkFinish(finish,game) ){
+		          alert('Gewonnen :)');
+
+	        }
+      }, 100);
+	  //render(game);
+
   });
 };
 
 render(game);
 // element "nesw"
-
-
-
-
-
-
-
