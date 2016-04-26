d04c9acb7369156e3c843a98ae634f233a8157a3
bklap <benjamin.kowatsch@gmail.com>
2016-04-26 11:43:15 +0200

    score hide mechanik

--- js/globals.js
+++ js/globals.js
@@ -1,106 +1,107 @@
-// var finish =
-//   [
-//   [
-//     {
-//       typ: "ne",
-//       orientation: 1
-//     },
-//     {
-//       typ: "nes",
-//       orientation: 1
-//     },
-//     {
-//       typ: "ne",
-//       orientation: 2
-//     },
-//   ],
-//   [
-//     {
-//       typ: "ne",
-//       orientation: 0
-//     },
-//     {
-//       typ: "nes",
-//       orientation: 3
-//     },
-//     {
-//       typ: "ne",
-//       orientation: 3
-//     }
-//   ]
-// ];
 var finish =
-    [
-      [
-        {
-          typ: "ne",
-          orientation: 1
-        },
-        {
-          typ: "ns",
-          orientation: 1
-        },
-        {
-          typ: "ns",
-          orientation: 1
-        },
-        {
-          typ: "ne",
-          orientation: 2
-        },
-      ],
-      [
-        {
-          typ: "ne",
-          orientation: 0
-        },
-        {
-          typ: "ne",
-          orientation: 2
-        },
-        {
-          typ: "ne",
-          orientation: 1
-        },
-        {
-          typ: "ne",
-          orientation: 3
-        },
-      ],
-      [
-        {
-          typ: "n",
-          orientation: 2
-        },
-        {
-          typ: "n",
-          orientation: 0
-        },
-        {
-          typ: "n",
-          orientation: 0
-        },
-        {
-          typ: "n",
-          orientation: 2
-        },
-      ],
-      [
-        {
-          typ: "n",
-          orientation: 0
-        },
-        {
-          typ: "n",
-          orientation: 1
-        },
-        {
-          typ: "n",
-          orientation: 3
-        },
-        {
-          typ: "n",
-          orientation: 0
-        }
-      ]
-    ];
+	[
+		[
+		   [
+			 {
+			   typ: "ne",
+			   orientation: 1
+			 },
+			 {
+			   typ: "nes",
+			   orientation: 1
+			 },
+			 {
+			   typ: "ne",
+			   orientation: 2
+			 },
+		   ],
+		   [
+			 {
+			   typ: "ne",
+			   orientation: 0
+			 },
+			 {
+			   typ: "nes",
+			   orientation: 3
+			 },
+			 {
+			   typ: "ne",
+			   orientation: 3
+			 }
+		   ]
+		 ],
+		[
+		  [
+			{
+			  typ: "ne",
+			  orientation: 1
+			},
+			{
+			  typ: "ns",
+			  orientation: 1
+			},
+			{
+			  typ: "ns",
+			  orientation: 1
+			},
+			{
+			  typ: "ne",
+			  orientation: 2
+			},
+		  ],
+		  [
+			{
+			  typ: "ne",
+			  orientation: 0
+			},
+			{
+			  typ: "ne",
+			  orientation: 2
+			},
+			{
+			  typ: "ne",
+			  orientation: 1
+			},
+			{
+			  typ: "ne",
+			  orientation: 3
+			},
+		  ],
+		  [
+			{
+			  typ: "n",
+			  orientation: 2
+			},
+			{
+			  typ: "n",
+			  orientation: 0
+			},
+			{
+			  typ: "n",
+			  orientation: 0
+			},
+			{
+			  typ: "n",
+			  orientation: 2
+			},
+		  ],
+		  [
+			{
+			  typ: "n",
+			  orientation: 0
+			},
+			{
+			  typ: "n",
+			  orientation: 1
+			},
+			{
+			  typ: "n",
+			  orientation: 3
+			},
+			{
+			  typ: "n",
+			  orientation: 0
+			}
+		  ]
+		]
+	];
--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 // Gibt eine Zufallszahl zwischen min (inklusive) und max (inklusive) zurück
 // Die Verwendung von Math.round() erzeugt keine gleichmäßige Verteilung!
+
+var stats = { level:0, score:0, end:true, last: null};
+
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min +1)) + min;
 }
@@ -10,6 +13,7 @@ var randomizeFinish = function( finish ){
 	tmpGame[i] = [];
     for(var j = 0; j < finish[i].length; j++){
 		var newObject = jQuery.extend({}, finish[i][j]);
+		newObject.visible = true;
 		newObject.orientation = getRandomInt(0,3);
 		tmpGame[i][j] = newObject;
     }
@@ -39,13 +43,18 @@ var checkFinish = function( finish,game ){
   return true;
 }
 
-var game = randomizeFinish(finish);
+var game = null;
 
 var spin = function(x){
-  var i = x.find('.rowid').val();
+	var i = x.find('.rowid').val();
     var j = x.find('.columnid').val();
     var elem = game[i][j];
+	elem.visible = false;
+	stats.last = {row: i, column: j};
     elem.orientation += 1;
+	stats.score++;
+	$('#spinCounter').html(stats.score);
+	x.addClass('show');
     if(elem.orientation > 3){
       elem.orientation = 0;
       x.addClass('o00');
@@ -54,35 +63,77 @@ var spin = function(x){
     }
 }
 
-var render = function(game){
+var render = function(){
   var html = '';
+	
+	if( stats.end ){
+		if( stats.level != 0 ){
+			html += '<div id="win">Sie haben das Spiel mit ' + stats.score + ' Spins gewonnen!!!!</div>'; 
+		}
+	
+		html += '<div id="startGame" class="btn">Spiel starten</div>';
+		
+		$('#game').html(html);
+		
+		$('#startGame').unbind('click').on('click', function(e){
+			stats.level = 0;
+		
+			game = randomizeFinish(finish[stats.level]);
+			stats.level++;
+			
+			$('#levelCounter').html(stats.level);
+			stats.end = false;
+			render();
+		});
+		
+	}
+	else{
+		 for(var i = 0; i < game.length; i++){
+			html += '<div class="row">';
+			for(var j = 0; j < game[i].length; j++){
+			  var showstr = game[i][j].visible ? 'show' : 'hide'; 
+			  if(stats.last){
+				if(i == stats.last.row && j == stats.last.column){
+					showstr = 'show';
+				}	
+			  }
+				
+			  html += '<div id="' + i + 'x' + j + '" class=" cell o' + game[i][j].orientation + ' ' + showstr + '">';
+			  html += '<input type="hidden" value="' + i + '" class="rowid" />';
+			  html += '<input type="hidden" value="' + j + '" class="columnid" />';
+			  html += '<img src="./img/' + game[i][j].typ + '.png"/></div>';
+			}
+			  html += '</div>';
+		  }
 
-  for(var i = 0; i < game.length; i++){
-    html += '<div class="row">';
-    for(var j = 0; j < game[i].length; j++){
-      html += '<div id="' + i + 'x' + j + '" class=" cell o' + game[i][j].orientation + '">';
-      html += '<input type="hidden" value="' + i + '" class="rowid" />';
-      html += '<input type="hidden" value="' + j + '" class="columnid" />';
-      html += '<img src="./img/' + game[i][j].typ + '.png"/></div>';
-    }
-      html += '</div>';
-  }
+		  $('#game').html(html);
 
-  $('#game').html(html);
+			$('#game .cell').unbind('click').on('click', function(e){
+			  $('#game .cell').unbind('click');
+			  spin($(this));
+			  setTimeout(function(){
+				  render(game);
+					if( checkFinish(finish[stats.level-1],game) ){
+						if( finish.length > stats.level ){
+							 game = randomizeFinish(finish[stats.level]);
+							 stats.level++;
+							 $('#levelCounter').html(stats.level);
+						} else {
+							//Spiel beendet
+							stats.end = true;
+							
+							
+						}
+						
+						render();
+					}
+			  }, 100);
+			  //render(game);
 
-    $('#game .cell').unbind('click').on('click', function(e){
-      $('#game .cell').unbind('click');
-      spin($(this));
-      setTimeout(function(){
-          render(game);
-	        if( checkFinish(finish,game) ){
-		          alert('Gewonnen :)');
-
-	        }
-      }, 100);
-	  //render(game);
-
-  });
+		  });
+	}
+	
+ 
 };
 
 render(game);
